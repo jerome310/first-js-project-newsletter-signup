453f11e908b0b015acbb498acef63df33c856d79
jerome310 <jeromeyoung333@gmail.com>
2023-06-25 17:10:21 -0400

    added feature for the subscribe button to take all the data and display the modal popup with the correct condition to email user

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ let displayValue = [];
 // Array version of email address feature
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+});
+
+subButton.addEventListener("click", () => {
   let inputValue = email.value;
   displayValue.push(inputValue);
   if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/)) {
@@ -20,31 +23,24 @@ form.addEventListener("submit", (e) => {
     popupEmail.textContent = "";
     subButton.disabled = true;
     parentModal.style.display = "none";
-    //subButton.disabled = true;
-    console.log("Button disabled")
   } else {
-    console.log("Hello Popup")
     email.style.backgroundColor = "";
     validText.style.color = "";
     validText.textContent = "";
     popupEmail.textContent = email.value;
-     subButton.addEventListener("click", () => {
-       parentModal.style.display = "block";
+    parentModal.style.display = "block";
 
-       // When the user clicks on <span> (x), close the modal
-       closeBtn.addEventListener("click", () => {
-         parentModal.style.display = "none";
-       });
+    // When the user clicks on <span> (x), close the modal
+    closeBtn.addEventListener("click", () => {
+      parentModal.style.display = "none";
+    });
 
-       // When the user clicks anywhere outside of the modal, close it
-       window.addEventListener("click", (event) => {
-         if (event.target == parentModal) {
-           parentModal.style.display = "none";
-         }
-       });
-     },{once : true});
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener("click", (event) => {
+      if (event.target == parentModal) {
+        parentModal.style.display = "none";
+      }
+    });
   }
-    // Need some logic to not display any value from the input field into the modal popup
-    // Also, I need to write a condition for the "subscribe to monthly newsletter button to no fire if there isn't a valid email address"
-    email.value = "";
+  email.value = "";
 });
