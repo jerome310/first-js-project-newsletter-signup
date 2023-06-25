97f74c2bc02c324eb77ebf06b258d39a6aa36d43
jerome310 <jeromeyoung333@gmail.com>
2023-06-25 00:43:20 -0400

    desktop - completely finished

--- script.js
+++ script.js
@@ -1,82 +1,50 @@
-const form = document.getElementById('form')
-const email = document.getElementById('user-email');
+const form = document.getElementById("form");
+const email = document.getElementById("user-email");
 const validText = document.querySelector(".valid-text");
-const emailText = document.querySelector('.email-text');
+const emailText = document.querySelector(".email-text");
+const popupEmail = document.querySelector(".popup-email");
+let parentModal = document.querySelector(".my-modal");
+let subButton = document.querySelector(".sub-button");
+let closeBtn = document.querySelector(".close");
 let displayValue = [];
 
 // Array version of email address feature
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let inputValue = email.value;
-    displayValue.push(inputValue);
-    console.log(displayValue)
-    if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/)) {
+  e.preventDefault();
+  let inputValue = email.value;
+  displayValue.push(inputValue);
+  if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/)) {
     email.style.backgroundColor = "#ffcccb";
     validText.style.color = "red";
-    validText.textContent = 'Valid email required';
-    } else {
-       email.style.backgroundColor = "";
-       validText.style.color = "";
-       validText.textContent = ""; 
-    }
-    email.value = ''
-});
-
-// Modal Popup
-
-// Get the modal
-let parentModal = document.querySelector(".my-modal");
-
-// Get the button that opens the modal
-let subButton = document.querySelector(".sub-button");
-
-// Get the <span> element that closes the modal
-let closeBtn = document.querySelector(".close");
-
-// When the user clicks on the button, open the modal
-subButton.addEventListener('click', () => {
-  parentModal.style.display = "block";
-
-// When the user clicks on <span> (x), close the modal
-  closeBtn.addEventListener("click", () => {
+    validText.textContent = "Valid email required";
+    popupEmail.textContent = "";
+    subButton.disabled = true;
     parentModal.style.display = "none";
-  });
+    //subButton.disabled = true;
+    console.log("Button disabled")
+  } else {
+    console.log("Hello Popup")
+    email.style.backgroundColor = "";
+    validText.style.color = "";
+    validText.textContent = "";
+    popupEmail.textContent = email.value;
+     subButton.addEventListener("click", () => {
+       parentModal.style.display = "block";
 
-// When the user clicks anywhere outside of the modal, close it
-  window.addEventListener("click", (event) => {
-    if (event.target == parentModal) {
-      parentModal.style.display = "none";
-    }
-  });
-})
+       // When the user clicks on <span> (x), close the modal
+       closeBtn.addEventListener("click", () => {
+         parentModal.style.display = "none";
+       });
 
-
-
-
-
-// String version of email address feature
-
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     let displayValue = '';
-//     let inputValue = email.value;
-//      displayValue = inputValue;
-//     if (!email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/)) {
-//     email.style.backgroundColor = "#ffcccb";
-//     validText.style.color = "red";
-//     validText.textContent = 'Valid email required';
-//     } else {
-//        email.style.backgroundColor = "";
-//        validText.style.color = "";
-//        validText.textContent = ""; 
-//     }
-//     email.value = ''
-// });
-
-
-
-// create a click event
-
-
-// Use the map method targetting the empty array
-// Use the new array name to display the data coming from the input tag inside the back tick html text that was part of the design
+       // When the user clicks anywhere outside of the modal, close it
+       window.addEventListener("click", (event) => {
+         if (event.target == parentModal) {
+           parentModal.style.display = "none";
+         }
+       });
+     },{once : true});
+  }
+    // Need some logic to not display any value from the input field into the modal popup
+    // Also, I need to write a condition for the "subscribe to monthly newsletter button to no fire if there isn't a valid email address"
+    email.value = "";
+});
